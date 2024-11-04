9426344a073b00ac6d99e1c31af6aad989f98d2f
frivolous-impulsor <ultrapilot95@gmail.com>
2024-11-03 21:49:53 -0500

    nonsense

--- schedule.c
+++ schedule.c
@@ -5,12 +5,11 @@
 #include <unistd.h>
 #include <time.h>
 #include <dirent.h>
+#include <stdbool.h>
 #include "indexMaxPriorityQueue.h"
 
 const int DAYS_IN_WEEK = 7;
 const int SLOTS_IN_DAY = 10;
-
-
 int needMatrix[DAYS_IN_WEEK][SLOTS_IN_DAY];   //needed number of people in each slot
 float hoursMatrix[DAYS_IN_WEEK][SLOTS_IN_DAY];    //hours in each slot in each day
 int updatedPQ[MAX_QUEUE_SLOT];
@@ -33,9 +32,95 @@ willTuple preferenceMatrix[DAYS_IN_WEEK][SLOTS_IN_DAY][16];
 float availableHoursArray[MAX_QUEUE_SLOT];  
 //each element with index i denotes the remaining available hours of the student with id i
 
+char *
+strtok_single (char * str, char const * delims)
+{
+  static char  * src = NULL;
+  char  *  p,  * ret = 0;
+
+  if (str != NULL)
+    src = str;
+
+  if (src == NULL)
+    return NULL;
+
+  if ((p = strpbrk (src, delims)) != NULL) {
+    *p  = 0;
+    ret = src;
+    src = ++p;
+
+  } else if (*src) {
+    ret = src;
+    src = NULL;
+  }
+
+  return ret;
+}
+
+int csv2array(char *filePath, char *array, int eleLength){
+    char buffer[255];
+    int i = 0; 
+    int j = 0;
+    FILE *file = fopen(filePath, "r");
+    if(file == NULL){
+        printf("no template found\n");
+        return 1;
+    }
+    fgets(buffer, sizeof(buffer), file);
+   
+    while(fgets(buffer, sizeof(buffer), file)){
+        char* token;
+        token = strtok_single(buffer, ",");
+        char tok[32];
+        strcpy(tok, token);
+        strcpy((array+14*eleLength*i + eleLength*j), tok);
+        i++;
+        while(token != NULL){
+            token = strtok_single(NULL, ",");
+            char tok[32];
+            strcpy(tok, token);
+            strcpy((array+14*eleLength*i + eleLength*j), tok);
+            i++;
+        }
+        j++;
+    }
+    return 0;
+}
+
+int templateRead(){
+    char templatePath[] = "ProcessData/schedule_template.csv";
+    char buffer[255];
+    bool validSlot[DAYS_IN_WEEK];
+    bool isValRow = false;
+    FILE *template = fopen(templatePath, "r");
+    if(template == NULL){
+        printf("no template found\n");
+        return 1;
+    }
+    printf("found template\n");
+    fgets(buffer, sizeof(buffer), template);
+    //enter the first slot
+    while (fgets(buffer, sizeof(buffer), template)){
+        //get tokens 
+        char* token;
+        token = strtok_single(buffer, ",");
+        while(token!=NULL){
+            printf(" <%s> ", token);
+
+            token = strtok_single(NULL, ",");
+        }
+        printf("\n");
+    }
+    fclose(template);
+    return 0;
+}
+
 int gatherCSVs(const char *dirName, char *files);
 
 int preprocessing(){
+    //fill in needMatrix
+
+
     //gather csv files from responses
     int fileCount;
     const char responsePath[] = "./ProcessData/responsesCSV";
@@ -46,7 +131,8 @@ int preprocessing(){
     printf("total csv files %d\n", fileCount);
     for(int i = 0; i< fileCount; i++){
         strcpy(filePath, files+i*MAX_NAME_LENGTH*2);
-        printf("file name: %s\n", filePath);
+        printf("start integrating file: %s\n", filePath);
+        //for each csv, 
     }
     
 
@@ -55,8 +141,6 @@ int preprocessing(){
 }
 //
 //TODO
-
-
 int arrange(){
     int day, slot, spot, peopleNeeded, id;
     float hours;
@@ -83,11 +167,14 @@ int arrange(){
     return 0;
 }
 
-
-
 int main(int argc, char* argv[])
 {   
-    preprocessing();
+    char filePath[] = "ProcessData/schedule_template.csv";
+    char *csvData = malloc(sizeof(char)*32*14*10);
+    csv2array(filePath, csvData, 32);
+    printf("%c",csvData[0]);
+    //preprocessing();
+    free(csvData);
     return 0;    
 }
 
