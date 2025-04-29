4980fa734128eb521427699346ae778a94748327
frivolous-impulsor <ultrapilot95@gmail.com>
2025-04-28 20:56:44 -0400

    CMake setup

---
+++ Shift_scheduling/build/CMakeFiles/Scheduling.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scheduling.dir/src/main.cpp.o"
+  "CMakeFiles/Scheduling.dir/src/main.cpp.o.d"
+  "Scheduling"
+  "Scheduling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Scheduling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Shift_scheduling/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/machine_1997/Program/Shift_scheduling
+# Build directory: /Users/machine_1997/Program/Shift_scheduling/Shift_scheduling/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
