8b53c175c1b71a0506467fdd7dae77c5b35f4a82
frivolous-impulsor <ultrapilot95@gmail.com>
2025-05-10 21:32:00 -0400

    slot half tested, common function for time manipulation awaits

--- include/slot.hpp
+++ include/slot.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 #include <set>
 #include <chrono>
 #include "commonFunctions.hpp"
@@ -10,20 +11,54 @@ class Slot{
 private:
     int m_dayIndex {};
     int m_slotIndex {};
-    time_t m_startTime {};
-    time_t m_endTime {};
+    std::chrono::time_point<std::chrono::system_clock> m_startTime {};
+    std::chrono::time_point<std::chrono::system_clock> m_endTime {};
     double m_duration {};     //duration in minutes
-    int m_peopleWanted {0};
+    int m_numPeopleWanted {0};
     bool m_critical {true};
-    std::set<int> m_peopleAssigned {};
+    std::set<int> m_peopleAssignedID {};
 public:
-    Slot(int dayI, int slotI, time_t startT, time_t endT)
+    Slot(int dayI, int slotI,   std::chrono::time_point<std::chrono::system_clock> startT, 
+                                std::chrono::time_point<std::chrono::system_clock> endT)
         :m_dayIndex {dayI}
         ,m_slotIndex {slotI}
         ,m_startTime {startT}
         ,m_endTime {endT}
         {
-            double timeDiffSec = difftime(endT, startT);
-            m_duration = timeDiffSec/60;
+            auto timeDiffSec = std::chrono::duration_cast<std::chrono::minutes>(endT - startT);
+            m_duration = timeDiffSec.count();
         }
+
+    double getDurationInMinute(){
+        return m_duration;
+    }
+
+    void setNumPeopleWanted(int n){
+        if(n<0){
+            throw std::invalid_argument("only non-negative number of people can be wanted");
+        }
+        m_numPeopleWanted = n;
+    }
+
+    void assignPersonID(int id){
+        m_peopleAssignedID.insert(id);
+    }
+
+    int getNumPeopleWanted(){
+        return m_numPeopleWanted;
+    }
+
+
+    std::set<int> getPeopleAssigned(){
+        return m_peopleAssignedID;
+    }
+
+    int getNumAssigned(){
+        return m_peopleAssignedID.size();
+    }
+
+    bool wantMorePeople(){
+        return this->getNumAssigned() < m_numPeopleWanted;
+    }
+
 };
--- tests/slotTest.cpp
+++ tests/slotTest.cpp
@@ -5,18 +5,68 @@
 
 TEST_CASE("worker class basic functions", "[worker]"){
     SECTION("construction"){
-        time_t start;
-        time(&start);
-        Slot s {0, 0, start, start};
+        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+        Slot s {0, 0, now, now};
         REQUIRE(true);
     }
 
+    
     SECTION("duration calculation"){
         std::string startTimeString {"2023-06-17 12:36:00"};
-        std::string endTimeString   {"2023-06-17 12:50:00"};
+        std::string endTimeString   {"2023-06-18 12:50:00"};
         std::string timeFormat      {"%Y-%m-%d %H:%M:%S"};
-        time_t startTime {ParseDateTime(startTime, timeFormat)};
-        time_t endTime {ParseDateTime(endTimeString, timeFormat)};
+
+        std::tm tmStart = {};
+        std::stringstream ssStart("2023-06-17 12:36:00");
+        ssStart >> std::get_time(&tmStart, "%Y-%m-%d %H:%M:%S");
+        auto tpStart = std::chrono::system_clock::from_time_t(std::mktime(&tmStart));
+
+        std::tm tmEnd = {};
+        std::stringstream ssEnd("2023-06-18 12:50:00");
+        ssEnd >> std::get_time(&tmEnd, "%Y-%m-%d %H:%M:%S");
+        auto tpEnd = std::chrono::system_clock::from_time_t(std::mktime(&tmEnd));
+
+        Slot s {0, 0, tpStart, tpEnd};
+        REQUIRE(s.getDurationInMinute() == 14 + 24*60);
     }
+    
+    SECTION("set num people wanted"){
+        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+        Slot s {0, 0, now, now};
+        s.setNumPeopleWanted(5);
+        s.setNumPeopleWanted(3);
+        s.setNumPeopleWanted(7);
+        REQUIRE(s.getNumPeopleWanted() == 7);
+        try
+        {
+            s.setNumPeopleWanted(-1);
+            REQUIRE(false);
+        }
+        catch(const std::invalid_argument& e)
+        {
+            REQUIRE(true);
+        }
+        
+    }
+
+    SECTION("assign id"){
+        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+        Slot s {0, 0, now, now};
+        REQUIRE(s.getNumPeopleWanted() == 0);
+        s.setNumPeopleWanted(3);
+        REQUIRE(s.getNumPeopleWanted() == 3);
+        REQUIRE(s.getNumAssigned() == 0);
+        s.assignPersonID(4);
+        REQUIRE(s.getNumAssigned() == 1);
+        REQUIRE(s.wantMorePeople());
+        s.assignPersonID(4);
+        REQUIRE(s.getNumAssigned() == 1);
+        REQUIRE(s.wantMorePeople());
+        s.assignPersonID(5);
+        s.assignPersonID(0);
+        REQUIRE(s.getNumAssigned() == 3);
+        REQUIRE(!s.wantMorePeople());
+    }
+
 
 }
